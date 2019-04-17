6b19128944bca7cbde80de8ef1c50df560e008fa
Osmanys Alonso <osmanys@gmail.com>
2019-04-17 00:33:40 -0400

    updt

---
+++ main/net/oz/TaskA.java
@@ -0,0 +1,10 @@
+package net.oz;
+
+import net.oz.io.InputReader;
+import net.oz.io.OutputWriter;
+
+public class TaskA {
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+        out.print(in.readInt());
+    }
+}
