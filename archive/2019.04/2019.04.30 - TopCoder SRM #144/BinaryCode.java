1b74f3a9757a73d8fcb97cec0ff683f2bf79daa3
Osmanys Alonso <osmanys@gmail.com>
2019-04-30 01:07:33 -0400

    TopCoder test

---
+++ archive/2019.04/2019.04.30 - TopCoder SRM #144/BinaryCode.java
@@ -0,0 +1,33 @@
+package net.oz;
+
+public class BinaryCode {
+    public String[] decode(String message) {
+        int last0 = 0;
+        int d, last1 = 0;
+        StringBuilder sb0 = new StringBuilder();
+        for(int i = 0; i < message.length(); i++){
+            sb0.append(last1);
+            d = message.charAt(i) - '0' - last0 - last1;
+            if(d < 0 || d > 1){
+                sb0 = new StringBuilder("NONE");
+                break;
+            }
+            last0 = last1;
+            last1 = d;
+        }
+        last0 = 0;
+        last1 = 1;
+        StringBuilder sb1 = new StringBuilder();
+        for(int i = 0; i < message.length(); i++){
+            sb1.append(last1);
+            d = message.charAt(i) - '0' - last0 - last1;
+            if(d < 0 || d > 1){
+                sb1 = new StringBuilder("NONE");
+                break;
+            }
+            last0 = last1;
+            last1 = d;
+        }
+        return new String[] { sb0.toString(), sb1.toString() };
+    }
+}
---
+++ lib/test/on2019_04/on2019_04_30_TopCoder_SRM__144/BinaryCode/Main.java
@@ -0,0 +1,14 @@
+package on2019_04.on2019_04_30_TopCoder_SRM__144.BinaryCode;
+
+import net.egork.chelper.tester.NewTopCoderTester;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Main {
+	@Test
+	public void test() throws Exception {
+		if (!NewTopCoderTester.test("lib/test/on2019_04/on2019_04_30_TopCoder_SRM__144/BinaryCode/BinaryCode.json"))
+			Assert.fail();
+	}
+}
